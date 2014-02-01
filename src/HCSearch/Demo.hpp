0c68b7a6a342a879d4e575bc0d22797e254966f1
Michael Lam <lamm@eecs.oregonstate.edu>
2014-02-01 09:48:31 -0800

    Abstracted out demo

---
+++ src/HCSearch/Demo.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <iostream>
+#include "../HCSearchLib/HCSearch.hpp"
+#include "MyProgramOptions.hpp"
+
+HCSearch::SearchSpace* setupSearchSpace();
+void demo(MyProgramOptions::ProgramOptions po);
--- src/HCSearch/Main.cpp
+++ src/HCSearch/Main.cpp
@@ -2,6 +2,7 @@
 #include "../HCSearchLib/HCSearch.hpp"
 #include "MyProgramOptions.hpp"
 #include "Main.hpp"
+#include "Demo.hpp"
 
 using namespace std;
 
@@ -34,108 +35,6 @@ int main(int argc, char* argv[])
 	// finalize
 	HCSearch::Setup::finalize();
     return 0;
-}
-
-HCSearch::SearchSpace* setupSearchSpace()
-{
-	// use standard CRF features for both heuristic and cost feature functions
-	HCSearch::IFeatureFunction* heuristicFeatFunc = new HCSearch::StandardFeatures();
-	HCSearch::IFeatureFunction* costFeatFunc = new HCSearch::StandardFeatures();
-
-	// use IID logistic regression as initial state prediction function
-	HCSearch::IInitialPredictionFunction* logRegInitPredFunc = new HCSearch::LogRegInit();
-
-	// use stochastic successor function
-	HCSearch::ISuccessorFunction* stochasticSuccessor = new HCSearch::StochasticSuccessor();
-
-	// use Hamming loss function
-	HCSearch::ILossFunction* lossFunc = new HCSearch::HammingLoss();
-
-	// construct search space from these functions that we specified
-	return new HCSearch::SearchSpace(heuristicFeatFunc, costFeatFunc, logRegInitPredFunc, stochasticSuccessor, lossFunc);
-}
-
-void demo(MyProgramOptions::ProgramOptions po)
-{
-	// time bound
-	int timeBound = po.timeBound;
-
-	// paths
-	string heuristicModelPath = HCSearch::Global::settings->paths->OUTPUT_HEURISTIC_MODEL_FILE;
-	string costModelPath = HCSearch::Global::settings->paths->OUTPUT_COST_H_MODEL_FILE;
-	string costOracleHModelPath = HCSearch::Global::settings->paths->OUTPUT_COST_ORACLE_H_MODEL_FILE;
-
-	// params
-	HCSearch::RankerType rankerType = HCSearch::ONLINE_RANK;
-
-	// datasets
-	vector< HCSearch::ImgFeatures* > XTrain;
-	vector< HCSearch::ImgLabeling* > YTrain;
-	vector< HCSearch::ImgFeatures* > XValidation;
-	vector< HCSearch::ImgLabeling* > YValidation;
-	vector< HCSearch::ImgFeatures* > XTest;
-	vector< HCSearch::ImgLabeling* > YTest;
-
-	// load dataset
-	HCSearch::Dataset::loadDataset(XTrain, YTrain, XValidation, YValidation, XTest, YTest);
-	const int numTestExamples = XTest.size();
-
-	// load search space functions and search space
-	HCSearch::SearchSpace* searchSpace = setupSearchSpace();
-
-	// load search procedure
-	HCSearch::ISearchProcedure* searchProcedure = new HCSearch::GreedySearchProcedure();
-
-	// train H
-	HCSearch::IRankModel* heuristicModel = HCSearch::Learning::learnH(XTrain, YTrain, XValidation, YValidation, timeBound, searchSpace, searchProcedure);
-
-	// train C
-	HCSearch::IRankModel* costModel = HCSearch::Learning::learnC(XTrain, YTrain, XValidation, YValidation, heuristicModel, timeBound, searchSpace, searchProcedure);
-
-	// train C with oracle H
-	HCSearch::IRankModel* costOracleHModel = HCSearch::Learning::learnCWithOracleH(XTrain, YTrain, XValidation, YValidation, timeBound, searchSpace, searchProcedure);
-
-	// save model
-	HCSearch::Model::saveModel(heuristicModel, heuristicModelPath, rankerType);
-	HCSearch::Model::saveModel(costModel, costModelPath, rankerType);
-	HCSearch::Model::saveModel(costOracleHModel, costOracleHModelPath, rankerType);
-
-	// to demonstrate loading...
-	delete heuristicModel;
-	delete costModel;
-	delete costOracleHModel;
-
-	// load model
-	heuristicModel = HCSearch::Model::loadModel(heuristicModelPath, rankerType);
-	costModel = HCSearch::Model::loadModel(costModelPath, rankerType);
-	costOracleHModel = HCSearch::Model::loadModel(costOracleHModelPath, rankerType);
-
-	// set up metadata for first test example
-	HCSearch::ISearchProcedure::SearchMetadata searchMetadata;
-	searchMetadata.saveAnytimePredictions = false;
-	searchMetadata.exampleName; //TODO
-	searchMetadata.iter; //TODO
-	searchMetadata.setType = HCSearch::TEST;
-
-	// infer LL
-	HCSearch::Inference::runLLSearch(XTest[0], YTest[0], timeBound, searchSpace, searchProcedure, searchMetadata);
-
-	// infer HL
-	HCSearch::Inference::runHLSearch(XTest[0], YTest[0], timeBound, searchSpace, searchProcedure, heuristicModel, searchMetadata);
-
-	// infer LC
-	HCSearch::Inference::runLCSearch(XTest[0], YTest[0], timeBound, searchSpace, searchProcedure, costOracleHModel, searchMetadata);
-
-	// infer HC
-	HCSearch::Inference::runHCSearch(XTest[0], timeBound, searchSpace, searchProcedure, heuristicModel, costModel, searchMetadata);
-
-	// clean up
-	delete searchSpace;
-	delete searchProcedure;
-	delete heuristicModel;
-	delete costModel;
-	delete costOracleHModel;
-	HCSearch::Dataset::unloadDataset(XTrain, YTrain, XValidation, YValidation, XTest, YTest);
 }
 
 HCSearch::SearchSpace* setupSearchSpace(MyProgramOptions::ProgramOptions po)
