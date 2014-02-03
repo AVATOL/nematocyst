2c0b776f47792ce638264036a7e679030088bb2b
Michael Lam <lamm@eecs.oregonstate.edu>
2014-02-03 00:01:35 -0800

    Doc

--- src/HCSearchLib/HCSearch.hpp
+++ src/HCSearchLib/HCSearch.hpp
@@ -24,7 +24,11 @@ using namespace std;
  * 
  * @section overview_sec HC-Search Overview
  * 
- * This section provides a brief overview about the HC-Search framework.
+ * HC-Search is a learning and inference framework for structured prediction. This is an implementation of HC-Search primarily geared toward scene labeling for computer vision, but may be adapted to other computer vision tasks.
+ *
+ * The HC-Search framework allows you to define a search space and search procedure to perform structured prediction. HC-Search learns a heuristic function to uncover high quality candidates and a cost function to select the best candidate.
+ *
+ * A search space consists of a heuristic feature function, cost feature function, initial state function, successor function and loss function. A search procedure can be anything that you define, including the commonly used greedy search and beam search. Our implementation allows you to define all of these components.
  * 
  * @section install_sec Downloading and Installing
  * 
