92169a6c94ca3fd9adeb4f197ad084ddcc471480
Michael Lam <lamm@eecs.oregonstate.edu>
2014-02-11 15:44:01 -0800

    Minor tweak to search space

--- src/HCSearchLib/SearchSpace.hpp
+++ src/HCSearchLib/SearchSpace.hpp
@@ -323,7 +323,7 @@ namespace HCSearch
 	 * Schedule to find the best cut for forming subgraphs. 
 	 * For each subgraph, flip its label to all possible classes.
 	 */
-	class CutScheduleSuccessor : public StochasticNeighborSuccessor
+	class CutScheduleSuccessor : public StochasticSuccessor
 	{
 	protected:
 
