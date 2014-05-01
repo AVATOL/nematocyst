710e68669d67ff3d11fbb7cb2afc15a87a2ed8dc
Michael Lam <lamm@eecs.oregonstate.edu>
2014-05-01 02:52:32 -0700

    Added Triple structure

--- src/HCSearchLib/MyPrimitives.hpp
+++ src/HCSearchLib/MyPrimitives.hpp
@@ -13,6 +13,7 @@ using namespace std;
 namespace MyPrimitives
 {
 	template <class T, class U> struct Pair;
+	template <class T, class U, class V> struct Triple;
 	template <class T> class Bimap;
 
 	/*!
@@ -32,6 +33,27 @@ namespace MyPrimitives
 
 		Pair() {}
 		~Pair() {}
+	};
+
+	/*!
+	 * Triple stores a basic 3-tuple
+	 */
+	template <class T, class U=T, class V=T>
+	struct Triple
+	{
+		T first;
+		U second;
+		V third;
+
+		Triple(T first, U second, V third)
+		{
+			this->first = first;
+			this->second = second;
+			this->third = third;
+		}
+
+		Triple() {}
+		~Triple() {}
 	};
 
 	/*!
