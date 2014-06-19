d011013c7b5374d0ff6de581af1e9774c3d4fb28
Michael Lam <lamm@eecs.oregonstate.edu>
2014-06-19 15:22:43 -0700

    Preprocess module cleaned up

--- preprocess/preprocess_grid_grayscale.m
+++ preprocess/preprocess_grid_grayscale.m
@@ -1,66 +1,64 @@
-function [ allData ] = preprocess( imagesPath, labelsPath, splitsPath, outputPath )
-%PREPROCESS Preprocesses folders of images and groundtruth labels into a
-%data format for HCSearch to work. Performs feature extraction.
-%
-%This implementation creates a regular grid of HOG/SIFT patches.
-%Therefore only need grayscale images.
+function [ allData ] = preprocess_grid_grayscale( imagesPath, labelsPath, color2label, splitsPath, outputPath )
+%PREPROCESS_GRID Preprocesses folders of images and groundtruth labels into a
+%data format for HCSearch to work. Performs feature extraction. 
+%This implementation creates a regular grid of HOG/SIFT patches on
+%grayscale images.
 %
 %One can change the features (e.g. add color) by editing this file.
 %
-%	imagesPath:	folder path to images folder of *.jpg images
-%                   e.g. 'DataRaw/SomeDataset/Images'
-%	labelsPath:	folder path to groundtruth folder of *.jpg label masks
-%                   e.g. 'DataRaw/SomeDataset/Annotations'
-%	splitsPath:	folder path that contains Train.txt,
-%               Validation.txt, Test.txt
-%                   e.g. 'DataRaw/SomeDataset/Splits'
-%	outputPath:	folder path to output preprocessed data
-%                   e.g. 'DataPreprocessed/SomeDataset'
+%	imagesPath:     folder path to images folder of *.jpg images
+%                       e.g. 'DataRaw/SomeDataset/Images'
+%	labelsPath:     folder path to groundtruth folder of *.jpg label masks
+%                       e.g. 'DataRaw/SomeDataset/Annotations'
+%   color2label:    mapping from groundtruth colors to label (use containers.Map)
+%	splitsPath:     folder path that contains Train.txt,
+%                   Validation.txt, Test.txt
+%                       e.g. 'DataRaw/SomeDataset/Splits'
+%	outputPath:     folder path to output preprocessed data
+%                       e.g. 'DataPreprocessed/SomeDataset'
 %
-%	allData:	data structure containing all preprocessed data
+%	allData:        data structure containing all preprocessed data
 
 %% argument checking
-narginchk(4, 4);
+narginchk(5, 5);
 
-%% parameters
+%% parameters - tune if necessary
 PATCH_SIZE = 32; % size of patches
 DESCRIPTOR = 1; % 0 = HOG, 1 = SIFT
-RGB_2_GRAY = 0; % set to 1 if input images are RGB (need to be grayscale)
-USE_BINARY = 1; % set to 1 if only extracting binary groundtruth
 
-%% define label map - NEED TO EDIT BELOW TO MAP GROUNDTRUTH COLORS TO LABELS
-% uses map container: index is (grayscale) color and value is label
-if USE_BINARY
-    labelMap = containers.Map({0, 255}, {-1, 1});
-else
-    labelMap = containers.Map({32 0 255 128 64}, {-1, 0, 1, 2, 3});
-end
-
-%% constants
+%% other settings
 TRAIN_LIST = 'Train.txt';
 VALID_LIST = 'Validation.txt';
 TEST_LIST = 'Test.txt';
-
 IMAGE_EXT = '.jpg';
 GROUNDTRUTH_EXT = '.jpg';
 
 %% process list of training, validation, test splits
-trainListFile = [splitsPath '/' TRAIN_LIST];
-validListFile = [splitsPath '/' VALID_LIST];
-testListFile = [splitsPath '/' TEST_LIST];
+trainListFile = [splitsPath filesep TRAIN_LIST];
+validListFile = [splitsPath filesep VALID_LIST];
+testListFile = [splitsPath filesep TEST_LIST];
 
+if ~exist(trainListFile, 'file')
+    error('training file does not exist: %s', trainListFile);
+end
 fid = fopen(trainListFile, 'r');
 list = textscan(fid, '%s');
 fclose(fid);
 trainList = list{1,1};
 trainRange = 1:length(trainList);
 
+if ~exist(validListFile, 'file')
+    error('validation file does not exist: %s', validListFile);
+end
 fid = fopen(validListFile, 'r');
 list = textscan(fid, '%s');
 fclose(fid);
 validList = list{1,1};
 validRange = length(trainList)+1:length(trainList)+length(validList);
 
+if ~exist(testListFile, 'file')
+    error('test file does not exist: %s', testListFile);
+end
 fid = fopen(testListFile, 'r');
 list = textscan(fid, '%s');
 fclose(fid);
@@ -77,8 +75,8 @@ for i= 1:length(fileArray)
     file = fileArray{i, 1};
     
     %% get paths
-    imgPath = [imagesPath '/' file IMAGE_EXT];
-    gtPath = [labelsPath '/' file GROUNDTRUTH_EXT];
+    imgPath = [imagesPath filesep file IMAGE_EXT];
+    gtPath = [labelsPath filesep file GROUNDTRUTH_EXT];
     fprintf('Processing image %s...\n', file);
     
     if ~exist(imgPath, 'file')
@@ -90,7 +88,7 @@ for i= 1:length(fileArray)
     
     %% get image
     img = imread(imgPath);
-    if RGB_2_GRAY
+    if ndims(img) == 3
         img = rgb2gray(img);
     end
     [img, height, width] = resize_image(img, PATCH_SIZE, PATCH_SIZE);
@@ -106,13 +104,13 @@ for i= 1:length(fileArray)
     
     %% get groundtruth
     labels = imread(gtPath);
-    if RGB_2_GRAY
+    if ndims(labels) == 3
         labels = rgb2gray(img);
     end
     [labels, ~, ~] = resize_image(labels, PATCH_SIZE, PATCH_SIZE);
     
     %% extract labels
-    [truthMatrix, labels] = pre_ground_truth(labels, PATCH_SIZE, labelMap);
+    [truthMatrix, labels] = pre_ground_truth(labels, PATCH_SIZE, color2label);
     
     %% get segments and locations
     [segments, segLocations, segSizes] = getSegments(PATCH_SIZE, height, width);
