eac68193f534ff4fbb69174c344bf658787d1cc5
Michael Lam <lamm@eecs.oregonstate.edu>
2014-07-18 19:33:40 -0700

    Annotation tool arguments simplified

--- utilities/annotation/annotate_images.m
+++ utilities/annotation/annotate_images.m
@@ -1,85 +1,61 @@
-function annotate_images( imagesPath, splitsPath, outputPath )
-%ANNOTATE_IMAGES Annotate characters using polygons
+function annotate_images( imagesPath, outputPath )
+%ANNOTATE_IMAGES Lightweight polyon annotation tool for characters.
+%
 %   imagesPath:     path to images
-%   splitsPath:     path to splits
 %   outputPath:     path to save annotations
 
 %% argument checking
-narginchk(3, 3);
+narginchk(2, 2);
 
 %% other settings
-TRAIN_LIST = 'Train.txt';
-VALID_LIST = 'Validation.txt';
-TEST_LIST = 'Test.txt';
-IMAGE_EXT = '.jpg';
+% valid image extensions to check - specify each beginning with .
+VALID_IMAGE_EXTENSIONS = lower({'.jpg'; '.png'});
 
-%% process list of training, validation, test splits
-trainListFile = [splitsPath filesep TRAIN_LIST];
-validListFile = [splitsPath filesep VALID_LIST];
-testListFile = [splitsPath filesep TEST_LIST];
-
-if ~exist(trainListFile, 'file')
-    error('training file does not exist: %s', trainListFile);
-end
-fid = fopen(trainListFile, 'r');
-list = textscan(fid, '%s');
-fclose(fid);
-trainList = list{1,1};
-trainRange = 1:length(trainList);
-
-if ~exist(validListFile, 'file')
-    error('validation file does not exist: %s', validListFile);
-end
-fid = fopen(validListFile, 'r');
-list = textscan(fid, '%s');
-fclose(fid);
-validList = list{1,1};
-validRange = length(trainList)+1:length(trainList)+length(validList);
-
-if ~exist(testListFile, 'file')
-    error('test file does not exist: %s', testListFile);
-end
-fid = fopen(testListFile, 'r');
-list = textscan(fid, '%s');
-fclose(fid);
-testList = list{1,1};
-testRange = length(trainList)+length(validList)+1:length(trainList)+length(validList)+length(testList);
-
-fileArray = [trainList; validList; testList];
-
+%% create output directory if doesn't exist
 if ~exist(outputPath, 'dir')
     mkdir(outputPath);
 end
 
+%% begin annotation
+ready = questdlg(['We will start annotating images. For each image a window will pop up accepting annotations. '...
+    'To add a point to the polygon, click on the image to set a point. Continue until you are done. '...
+    'When you are done, press the [ENTER] key or right click anywhere on the image to exit. '...
+    '(Note that the program will automatically connect the last point to the first point!) '...
+    'From there, accept or reject the annotation, give it a character ID and state, then continue. '...
+    'Are you ready?']);
+if strcmp(ready, 'Yes') ~= 1
+    fprintf('Exiting prematurely...\n');
+    return; 
+end
+
 %% process images/groundtruth folder
-nImages = length(fileArray);
-cnt = 1;
-for i= 1:length(fileArray)
-    file = fileArray{i, 1};
+fileListingArray = dir(imagesPath);
+for i= 1:length(fileListingArray)
+    fileStruct = fileListingArray(i);
     
-    %% get paths
-    imgPath = [imagesPath filesep file IMAGE_EXT];
-    fprintf('Processing image %s...\n', file);
-    
-    if ~exist(imgPath, 'file')
-        error(['image "' imgPath '" does not exist']);
+    %% skip folders and files with invalid extensions
+    if fileStruct.isdir
+        continue;
+    else
+        [~,fileName,ext] = fileparts(fileStruct.name);
+        if sum(ismember(VALID_IMAGE_EXTENSIONS, lower(ext))) == 0
+            continue;
+        end
     end
     
-    %% get image
+    %% begin user interactive annotation session
+    imgPath = [imagesPath filesep fileStruct.name];
     img = imread(imgPath);
-    
-    %% annotate polygons
     objects = annotate_image_polygon(img);
     
-    %% save
-    save_annotation(objects, [outputPath filesep file '.txt']);
-    
-    cnt = cnt+1;
+    %% save annotation data to file
+    save_annotation(objects, [outputPath filesep fileName '.txt']);
 end
 
 end
 
 function save_annotation(objects, filename)
+% saves annotation objects to file
 % format: x1,y1;...;xn,yn:charID:charState
 
 fid = fopen(filename, 'w');
