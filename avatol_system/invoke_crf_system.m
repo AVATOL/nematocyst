6c8c924a0aaf6bd1415aec3ee57034b11044aebd
Michael Lam <lamm@eecs.oregonstate.edu>
2014-07-23 18:48:15 -0700

    Invoke system stub

---
+++ avatol_system/invoke_crf_system.m
@@ -0,0 +1,111 @@
+function invoke_crf_system( inputPath, outputPath, options )
+%INVOKE_SYSTEM Use this function to invoke the entire character scoring
+%system.
+%
+%   inputPath   : path/.../sorted_input_data_<charID>_<charName>.txt
+%   outputPath  : path/.../sorted_output_data_<charID>_<charName>.txt
+
+%% ========== argument checking
+narginchk(2, 3);
+
+%% ========== default optional arguments
+if nargin < 3
+    options = struct;
+end
+if ~isfield(options, 'DATASET_PATH')
+    options.DATASET_PATH = '';
+end
+if ~isfield(options, 'TEMP_PATH')
+    options.TEMP_PATH = 'temp-ignore';
+end
+if ~isfield(options, 'LOG_FILE')
+    options.LOG_FILE = [options.TEMP_PATH filesep 'log.txt'];
+end
+if ~isfield(options, 'PREPROCESSED_PATH')
+    options.PREPROCESSED_PATH = [options.TEMP_PATH filesep 'predata'];
+end
+
+%% ========== begin
+if ~exist(options.TEMP_PATH, 'dir')
+    mkdir(options.TEMP_PATH);
+end
+log_fid = fopen(options.LOG_FILE, 'a');
+
+tglobalstart = tic;
+writelog(log_fid, '==========\n');
+writelog(log_fid, sprintf('Begin AVATOL system at %s.\n\n', datestr(now)));
+
+%% ========== parse arguments of input file
+writelog(log_fid, 'Parsing input file...\n');
+
+% get character ID from file name
+[inPath, inFileName, inExt] = fileparts(inputPath);
+charID = get_char_id_from_file_name(inFileName);
+
+% get list of training and test instances
+[trainingList, scoringList] = parse_input_file(inputPath);
+
+telapsed = toc(tglobalstart);
+writelog(log_fid, sprintf('Finished parsing input file. (%.1fs)\n\n', telapsed));
+
+%% ========== preprocess data for HC-Search
+tstart = tic;
+writelog(log_fid, 'Preprocessing input data...\n');
+
+% extract features, preprocess into data for HC-Search
+color2label = containers.Map({0, 255}, {-1, 1});
+allData = preprocess_avatol('sample-ignore', trainingList, scoringList, ...
+    charID, color2label, options.PREPROCESSED_PATH);
+
+telapsed = toc(tstart);
+writelog(log_fid, sprintf('Finished preprocessing input data. (%.1fs)\n\n', telapsed));
+
+%% ========== call HC-Search
+tstart = tic;
+writelog(log_fid, 'Running character detection...\n');
+
+% if ispc
+%     [status, result] = dos(''); % TODO
+% else
+%     [status, result] = unix(''); % TODO
+% end
+
+telapsed = toc(tstart);
+writelog(log_fid, sprintf('Finished running character detection. (%.1fs)\n\n', telapsed));
+
+%% ========== postprocess data for character scoring
+tstart = tic;
+writelog(log_fid, 'Running character scoring...\n');
+
+% postprocess TODO
+
+telapsed = toc(tstart);
+writelog(log_fid, sprintf('Finished running character scoring. (%.1fs)\n\n', telapsed));
+
+%% ========== save scores
+tstart = tic;
+writelog(log_fid, 'Saving scores...\n');
+
+% save scores TODO
+[outPath, outFileName, outExt] = fileparts(outputPath);
+
+telapsed = toc(tstart);
+writelog(log_fid, sprintf('Finished saving scores. (%.1fs)\n', telapsed));
+
+%% ========== done
+telapsed = toc(tglobalstart);
+writelog(log_fid, sprintf('\nFinished AVATOL system at %s. Total elapsed time: %.1fs\n', datestr(now), telapsed));
+writelog(log_fid, '==========\n\n');
+
+fclose(log_fid);
+
+end
+
+function charID = get_char_id_from_file_name(inFileName)
+
+FILE_PREFIX = 'sorted_input_data_';
+
+charID = inFileName(length(FILE_PREFIX)+1:end);
+charID(strfind(charID, '_'):end) = [];
+
+end
