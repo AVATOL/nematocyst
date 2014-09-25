3abced8ae55160f3bbe49a6af645e55ad6dc5eaa
Michael Lam <lamm@eecs.oregonstate.edu>
2014-09-25 11:57:26 -0700

    Updated output delimiters

--- avatol_system/postprocess/write_scores.m
+++ avatol_system/postprocess/write_scores.m
@@ -16,21 +16,32 @@ function write_scores( outputPath, trainingList, scoringList, nonScoringList )
 %                       a non-scored test instance and each struct has
 %                           .pathToMedia: path to image
 
+%% constants
+DATA_DELIMITER = '|';
+
+%% write to file
 fid = fopen(outputPath, 'w');
 
 for i = 1:length(trainingList)
     data = trainingList{i};
-    fprintf(fid, 'training_data:%s:%s:%s\n', data.pathToMedia, data.charState, data.pathToAnnotation);
+%     fprintf(fid, 'training_data|%s|%s|%s\n', data.pathToMedia, data.charState, data.pathToAnnotation);
+    fprintf(fid, 'training_data%s%s', DATA_DELIMITER, data.pathToMedia);
+    fprintf(fid, '%s%s', DATA_DELIMITER, data.charState);
+    fprintf(fid, '%s%s\n', DATA_DLIMITER, data.pathToAnnotation);
 end
 
 for i = 1:length(scoringList)
     data = scoringList{i};
-    fprintf(fid, 'image_scored:%s:%s:%s\n', data.pathToMedia, data.charState, data.pathToDetection);
+%     fprintf(fid, 'image_scored|%s|%s|%s\n', data.pathToMedia, data.charState, data.pathToDetection);
+    fprintf(fid, 'image_scored%s%s', DATA_DELIMITER, data.pathToMedia);
+    fprintf(fid, '%s%s', DATA_DELIMITER, data.charState);
+    fprintf(fid, '%s%s\n', DATA_DELIMITER, data.pathToDetection);
 end
 
 for i = 1:length(nonScoringList)
     data = nonScoringList{i};
-    fprintf(fid, 'image_not_scored:%s\n', data.pathToMedia);
+%     fprintf(fid, 'image_not_scored|%s\n', data.pathToMedia);
+    fprintf(fid, 'image_not_scored%s%s\n', DATA_DELIMITER, data.pathToMedia);
 end
 
 fclose(fid);
